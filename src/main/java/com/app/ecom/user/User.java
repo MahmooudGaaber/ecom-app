4d6d3b71c82ea0d7ccd7ff385a31fefe960ee39f
MahmooudGaaber <mahmoud.gr231@gmail.com>
2025-11-21 10:03:21 +0200

    Sec 5 : Setup Data JPA On User and Create User Table

--- src/main/java/com/app/ecom/user/User.java
+++ src/main/java/com/app/ecom/user/User.java
@@ -1,9 +1,32 @@
 package com.app.ecom.user;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Entity
+// * Replace of No Args Constructor Provided From LOMBOK
+@NoArgsConstructor
+// * Replace of All Args Constructor Provided From LOMBOK
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
     private String firstName;
     private String lastName;
+
+    // ? Mandatory Empty Constructor to active Jpa
+    // * uses reflection to create entity objects
+    // * so i need to add no Arg Constructor or Create one Manually
+
+
+
 }
