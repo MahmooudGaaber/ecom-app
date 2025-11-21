f5ffd4cc3756c3643889bf78173f465ebfc1c84f
MahmooudGaaber <mahmoud.gr231@gmail.com>
2025-11-21 13:44:04 +0200

    Sec 5 : Final Touch on Repository class

--- src/main/java/com/app/ecom/user/UserRepository.java
+++ src/main/java/com/app/ecom/user/UserRepository.java
@@ -4,9 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 // ? Repository Usage
-// * it's an easy way to setup all queries that i will write
+// * it's an easy way to Set up all queries that I will write
 // * it's only take 2 parameters
-// * ( User ) it's the data class that i will execute queries on
+// * ( User ) it's the data class that I will execute queries on
 // * ( Long ) the data type of the Primary Key
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {}
