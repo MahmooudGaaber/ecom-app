d0fdbdc1cd232f481189fed5de6e94f4e0e7be78
MahmooudGaaber <mahmoud.gr231@gmail.com>
2025-11-21 16:29:11 +0200

    Sec 5 : Rehandle Service File by using JPA

--- src/main/java/com/app/ecom/user/UserService.java
+++ src/main/java/com/app/ecom/user/UserService.java
@@ -1,40 +1,38 @@
 package com.app.ecom.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
-    long usersId = 1L;
-    private List<User> userList = new ArrayList<>();
+    private final UserRepository userRepository;
+
+
 
 
     public List<User> fetchAllUsers(){
-        return userList;
+        return userRepository.findAll();
     }
 
-    public List<User> addUser( User newUser){
-        newUser.setId( usersId++ );
-        userList.add(newUser);
-        return userList;
+    public void addUser( User newUser){
+        userRepository.save(newUser);
     }
 
     public Optional<User> getUser(long id) {
-        return   userList.stream().filter(
-                item -> item.getId().equals(id)
-        ).findFirst();
+        return userRepository.findById(id);
     }
 
     public boolean updateUser (long id , User updatedUser) {
-      return  userList.stream()
-              .filter(
-              item -> item.getId().equals(id)
-      ).findFirst()
+      return  getUser(id)
               .map(existingUser -> {
                   existingUser.setFirstName(updatedUser.getFirstName());
                   existingUser.setLastName(updatedUser.getLastName());
+                  userRepository.save(existingUser);
                   return true;
               }).orElse(false);
     }
